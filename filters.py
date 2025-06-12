adfcd9f8ad71e2a473b182af3f89d1be96cb7eab
Scsant <110061187+Scsant@users.noreply.github.com>
2025-06-12 12:04:56 -0300

    Update filters.py

--- filters.py
+++ filters.py
@@ -1,35 +1,44 @@
+#filters.py
 import streamlit as st
 import pandas as pd
 from datetime import date
 
 def apply_filters(df):
+    df_filtered = df.copy()
+    df_filtered['data_cto'] = pd.to_datetime(df_filtered['data_cto'], errors='coerce')
+    df_filtered = df_filtered.dropna(subset=['data_cto'])
+
     st.sidebar.header('Filtros')
 
-    regioes = st.sidebar.multiselect('Região', df['dcr_regiao'].unique())
-    projetos = st.sidebar.multiselect('Projeto', df['nom_projeto'].unique())
-    operacoes = st.sidebar.multiselect('Operação', df['dcr_operacao'].unique())
+    regioes = st.sidebar.multiselect('Região', df_filtered['dcr_regiao'].unique())
+    projetos = st.sidebar.multiselect('Projeto', df_filtered['nom_projeto'].unique())
+    operacoes = st.sidebar.multiselect('Operação', df_filtered['dcr_operacao'].unique())
 
-    df['data_cto'] = pd.to_datetime(df['data_cto'], errors='coerce')
-    df = df.dropna(subset=['data_cto'])
-
-    if not df.empty:
-        data_min_value = df['data_cto'].min().date()
-        data_max_value = df['data_cto'].max().date()
+    if not df_filtered.empty:
+        data_min_value = df_filtered['data_cto'].min().date()
+        data_max_value = df_filtered['data_cto'].max().date()
     else:
-        data_min_value = date.today()
-        data_max_value = date.today()
+        data_min_value = data_max_value = date.today()
 
     data_min = st.sidebar.date_input('Data Inicial', data_min_value)
     data_max = st.sidebar.date_input('Data Final', data_max_value)
 
+    if data_min > data_max:
+        st.sidebar.error("A data inicial não pode ser maior que a data final.")
+        return df_filtered[0:0]
+
     if regioes:
-        df = df[df['dcr_regiao'].isin(regioes)]
+        df_filtered = df_filtered[df_filtered['dcr_regiao'].isin(regioes)]
     if projetos:
-        df = df[df['nom_projeto'].isin(projetos)]
+        df_filtered = df_filtered[df_filtered['nom_projeto'].isin(projetos)]
     if operacoes:
-        df = df[df['dcr_operacao'].isin(operacoes)]
+        df_filtered = df_filtered[df_filtered['dcr_operacao'].isin(operacoes)]
 
-    df = df[(df['data_cto'] >= pd.to_datetime(data_min)) & (df['data_cto'] <= pd.to_datetime(data_max))]
+    df_filtered = df_filtered[
+        (df_filtered['data_cto'] >= pd.to_datetime(data_min)) &
+        (df_filtered['data_cto'] <= pd.to_datetime(data_max))
+    ]
 
-    return df
+    return df_filtered
+
 
